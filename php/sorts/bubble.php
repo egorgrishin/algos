366cc1cab862181ccc16fe98ac2c70dfc649ff6d
Egor Grishin <egor-grishin@bk.ru>
2024-08-08 20:07:11 +0300

    bubble sort

--- php/sorts/bubble.php
+++ php/sorts/bubble.php
@@ -5,11 +5,14 @@ function bubbleSort(array $array, callable $cmp): array
 {
     $count = count($array);
     for ($i = 0; $i < $count; $i++) {
+        $isSorted = true;
         for ($j = $i + 1; $j < $count; $j++) {
             if ($cmp($array[$i], $array[$j])) {
                 [$array[$i], $array[$j]] = [$array[$j], $array[$i]];
+                $isSorted = false;
             }
         }
+        if ($isSorted) break;
     }
     return $array;
 }
